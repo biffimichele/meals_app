b6d3a7c55e745aaa8e3e211dee01c2ffbe71a2ec
Michele Biffi <michelebiffi98@gmail.com>
2025-03-21 00:45:53 +0100

    created category page with grid layout and dummy data, added inkwell to categories to make them tappable

---
+++ lib/screens/categories.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../data/dummy_data.dart';
+import '../widgets/category_grid_items.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category!'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
